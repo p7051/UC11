9c1e6a30e7733cee596645c36f2740290971ff10
Pedro <pedrokrieserbock@gmail.com>
2023-09-21 14:30:54 -0300

    Commit #2: Implementada a função listarProdutosVendidos() em ProdutosDAO.

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -72,6 +72,49 @@ public class ProdutosDAO {
         return produtos;
     }
     
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+        ArrayList<ProdutosDTO> produtosVendidos = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement prep = null;
+        ResultSet resultSet = null;
+
+        try {
+            conn = new conectaDAO().connectDB();
+            String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+            prep = conn.prepareStatement(sql);
+            resultSet = prep.executeQuery();
+
+            while (resultSet.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultSet.getInt("id"));
+                produto.setNome(resultSet.getString("nome"));
+                produto.setValor(resultSet.getInt("valor"));
+                produto.setStatus(resultSet.getString("status"));
+                produtosVendidos.add(produto);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // Feche a conexão e os recursos
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (prep != null) {
+                    prep.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return produtosVendidos;
+    }
+
+    
     public void venderProduto(int produtoId) {
     Connection conn = null;
     PreparedStatement prep = null;
